1720c0104c8e95effab444e00a3efa1db5138732
PatricCormac <socolov1897@gmail.com>
2020-10-27 22:50:24 +0500

    Фильтр по типу жилья

--- js/pin.js
+++ js/pin.js
@@ -3,7 +3,6 @@
 (() => {
   const adForm = document.querySelector(`.ad-form`);
   const fieldsets = document.querySelectorAll(`fieldset`);
-  const tempPin = document.querySelector(`#pin`).content;
   const mainPin = document.querySelector(`.map__pin--main`);
   const mapElement = document.querySelector(`.map`);
   const pinsList = document.querySelector(`.map__pins`);
@@ -13,15 +12,13 @@
   let mainPinLocationX = Math.round(parseInt(mainPin.style.left, 10) + parseInt(MAIN_PIN_WIDTH, 10) / 2);
   let mainPinLocationY = Math.round(parseInt(mainPin.style.top, 10) + parseInt(MAIN_PIN_HEIGHT, 10) / 2);
 
-  const fragment = document.createDocumentFragment();
-
   for (let i = 0; i < fieldsets.length; i++) {
     fieldsets[i].disabled = true;
   }
 
   const onActivePage = () => {
     mapElement.classList.remove(`map--faded`);
-    pinsList.appendChild(fragment);
+    pinsList.appendChild(window.render.pins);
     adForm.classList.remove(`ad-form--disabled`);
     document.querySelector(`[name="address"]`).value = `${mainPinLocationX} ${mainPinLocationY}`;
     for (let i = 0; i < fieldsets.length; i++) {
@@ -40,21 +37,4 @@
       onActivePage();
     }
   });
-
-  const renderAds = (ad) => {
-    const adElement = tempPin.cloneNode(true);
-    adElement.querySelector(`.map__pin`).style =
-      `left: ${ad.location.x}px; top: ${ad.location.y}px;`;
-    adElement.querySelector(`img`).src = ad.author.avatar;
-    adElement.querySelector(`img`).alt = ad.offer.title;
-    return adElement;
-  };
-
-  window.pin = {
-    appEndAds: (arr) => {
-      arr.map((ad) => {
-        fragment.append(renderAds(ad));
-      });
-    }
-  };
 })();
---
+++ js/render.js
@@ -0,0 +1,52 @@
+'use strict';
+
+(() => {
+  const MAX_COUNT_PINS = 5;
+  const houseTypeElement = document.querySelector(`#type`);
+  const tempPin = document.querySelector(`#pin`).content;
+  const pinsList = document.querySelector(`.map__pins`);
+  const mainPin = document.querySelector(`.map__pin--main`);
+
+  let houseType;
+  let ads = [];
+  const fragment = document.createDocumentFragment();
+
+  const renderAds = (ad) => {
+    const adElement = tempPin.cloneNode(true);
+    adElement.querySelector(`.map__pin`).style =
+      `left: ${ad.location.x}px; top: ${ad.location.y}px;`;
+    adElement.querySelector(`img`).src = ad.author.avatar;
+    adElement.querySelector(`img`).alt = ad.offer.title;
+    return adElement;
+  };
+
+  const appEndAds = (arr) => {
+    for (let i = 0; i < arr.length && i < MAX_COUNT_PINS; i++) {
+      fragment.append(renderAds(arr[i]));
+    }
+  };
+
+  window.backend.load((data) => {
+    ads = data;
+    appEndAds(ads);
+  });
+
+  const updateAds = (filteredAds) => {
+    appEndAds(filteredAds);
+    pinsList.innerHTML = ``;
+    pinsList.appendChild(mainPin);
+    pinsList.appendChild(fragment);
+  };
+
+  houseTypeElement.addEventListener(`change`, () => {
+    houseType = houseTypeElement.value;
+    let filteredAds = ads.filter((ad) => {
+      return ad.offer.type === houseType;
+    });
+    updateAds(filteredAds);
+  });
+
+  window.render = {
+    pins: fragment
+  };
+})();
